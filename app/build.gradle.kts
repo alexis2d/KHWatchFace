4e065a380d11acf43aed17f36bbad81f5d4f2909
Alexis Duquenet <alexisduquenet@gmail.com>
2025-08-18 22:32:59 +0200

    Changes for build bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.khwatchface"
+    namespace = "com.watchface.khwatchface"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.khwatchface"
+        applicationId = "com.watchface.khwatchface"
         minSdk = 34
         targetSdk = 36
         versionCode = 1
